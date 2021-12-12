d019b7723d748c28f192e81afd434c55b87f9868
Sasa Cvetkovic <sasa.cvetkovic@yahoo.com>
2021-12-12 14:11:21 +0100

    Day 12 :(

---
+++ day12/day12.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+
+def getData():
+    f = open('input0.txt')
+    data = f.read().splitlines()
+    f.close()
+    return data
+
+
+def count_distinct_paths(graph, can_duplicate):
+    # Keeping path + can duplicate further
+    paths_list = [(('start',), not can_duplicate)]
+
+    distinct = 0
+    while paths_list:
+        # get last one and check if we are at end
+        print(paths_list)
+        path, duplicate = paths_list.pop()
+        if path[-1] == 'end':
+            distinct += 1
+        else:
+            for neighbor in graph[path[-1]]:
+                if neighbor.isupper() or neighbor not in path:
+                    paths_list.append((path + (neighbor,), duplicate))
+                elif not duplicate and neighbor != 'start':
+                    paths_list.append((path + (neighbor,), True))
+    return distinct
+
+
+if __name__ == '__main__':
+    data = getData()
+
+    input_list = [line.split('-') for line in data]
+    graph = {}
+    for input in input_list:
+        k, v = input[:2]
+
+        # This was my mistake - I had only k directions...
+        graph.setdefault(k, []).append(v)
+        graph.setdefault(v, []).append(k)
+
+    print(f'Part 1 {count_distinct_paths(graph, False)}')
+    print(f'Part 2 {count_distinct_paths(graph, True)}')
